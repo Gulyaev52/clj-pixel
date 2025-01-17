c8c48aae1c42da8bb466ebce4e06f81845dcceb8
Kirill Gulyaev <gulyaev52@gmail.com>
2025-01-17 12:12:53 +0700

    move colorPicker.js and swatch.js near color-picker

--- src/pixel_art/measure.cljs
+++
@@ -1,8 +0,0 @@
-(ns pixel-art.measure)
-
-(defn measure-fn [label fn]
-  (let [start (. js/Date now)
-        res (fn)
-        end (. js/Date now)]
-    (println (str label ": " (- end start) "ms"))
-    res))
