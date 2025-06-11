d8aadd44b2811048106e77581b7295c221dac02d
Kirill Gulyaev <gulyaev52@gmail.com>
2025-06-11 15:19:25 +0700

    fix resize-pixel

--- src/pixel_art/tool/utils.cljs
+++ src/pixel_art/tool/utils.cljs
@@ -51,11 +51,15 @@
 
 ;; Resize the pixel at {col, row} for the provided size. Will return the array of pixels centered
 ;; * around the original pixel, forming a pixel square of side=size
+;; todo: refactoring
 (defn resize-pixel [point size]
-  (for [j (range 0 size)
-        i (range 0 size)]
-    #js [(+ (- (aget point 0) (. js/Math (floor (/ size 2)))) i)
-         (+ (- (aget point 1) (. js/Math (floor (/ size 2)))) j)]))
+  (when point
+    (let [x (or (aget point 0) (:x point))
+          y (or (aget point 1) (:y point))]
+      (for [j (range 0 size)
+            i (range 0 size)]
+        #js [(+ (- x (. js/Math (floor (/ size 2)))) i)
+             (+ (- y (. js/Math (floor (/ size 2)))) j)]))))
 
 (defn with-highlight-cel-under-cursor [events-handlers]
   (merge
@@ -64,22 +68,20 @@
     (fn [db event]
       (let [handler-res (if-let [f (:mouse-move events-handlers)]
                           (f db event)
-                          {:db db})]
-        (if-not (:initial-mouse-down-pos db)
-          (let [{:keys [pixel-size]} (get-tool-options db)
-                points (if pixel-size
-                         (resize-pixel (:pos event) pixel-size)
-                         [(:pos event)])
-                size (-> db :sprite :size)
+                          {:db db})
+            {:keys [pixel-size]} (get-tool-options db)
+            points (if pixel-size
+                     (resize-pixel (:pos event) pixel-size)
+                     [(:pos event)])
+            size (-> db :sprite :size)
 
-                visual-effects (get-empty-visual-effects db)
-                current-cel (get-current-cel db)]
-            (doseq [pos points]
-              (let [color (color/get-highlight-color (cel/get-pixel pos current-cel))]
-                (aset visual-effects (geometry/pos->idx (:x pos) (:y pos) size) color)))
-            {:db (-> (or (:db handler-res) db)
-                     (assoc :visual-effects visual-effects))})
-          {:db db})))
+            visual-effects (get-empty-visual-effects db)
+            current-cel (get-current-cel db)]
+        (doseq [pos points]
+          (let [color (color/get-highlight-color (cel/get-pixel pos current-cel))]
+            (aset visual-effects (geometry/pos->idx (aget pos 0) (aget pos 1) size) color)))
+        {:db (-> (or (:db handler-res) db)
+                 (assoc :visual-effects visual-effects))}))
     :mouse-down
     (fn [db event]
       (let [handler-res (when-let [f (:mouse-down events-handlers)]
